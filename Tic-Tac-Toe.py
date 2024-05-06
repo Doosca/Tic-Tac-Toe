00ce29f3b314187c9571d554dbe9c2d97e380c98
Doosca <Cookiegurl0908@gmail.com>
2024-05-06 14:12:48 -0700

    new commit

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,1 +1,6 @@
-print('**Lets make cookies!** A text adventure game! Are you ready?')
+print('***Lets make cookies!*** A text adventure game! Are you ready?')
+ready_or_not = input()
+if ready_or_not == "yes":
+    print("Okay! Lets get started")
+else:
+    print('...well get outta here then!')
